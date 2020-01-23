c58af176506b7674a365293a67951aaac64e319c
Dhruv Krishnan <dhruvkrishnan@gmail.com>
2020-01-23 10:50:38 -0800

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,7 +1,11 @@
-#inotifywait --format '%w%f' -e modify toInflux_*_.out; do echo $file; done
+touch toInflux_dummy_.out
+curl -i -XPOST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE grafana"
 while true; do
     echo "In execute.sh"
     filename=$(inotifywait --format '%w%f' -e modify toInflux_*_.out)
     echo $filename
     sh InfluxDB.sh $filename
+    if test -f "toInflux_dummy_.out"; then
+        rm toInflux_dummy_.out
+    fi
 done
