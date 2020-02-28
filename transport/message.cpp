e4164729a51b428ab4db1c58dbe4ed388b44a4e6
RohanSogani <ronsogani@gmail.com>
2020-02-28 12:16:38 -0800

    Sharding Message changes

--- transport/message.cpp
+++ transport/message.cpp
@@ -497,9 +497,18 @@ void YCSBClientQueryMessage::copy_from_buf(char *buf)
 		assert(req->key < g_synth_table_size);
 		requests.add(req);
 	}
-
+	
+	size_t sizeOfShards;
+	COPY_VAL(sizeOfShards, buf, ptr);
+	shards_involved.init(sizeOfShards);
+	for(uint64_t i = 0; i < sizeOfShards; i++)
+	{
+		DEBUG_M("YCSBClientQueryMessage::copy ycsb_request alloc shards\n");
+		uint64_t shard;
+		COPY_VAL(shard, buf, ptr);
+		shards_involved.add(shard);
+	}
 	COPY_VAL(return_node, buf, ptr);
-
 	assert(ptr == get_size());
 }
 
@@ -517,6 +526,17 @@ void YCSBClientQueryMessage::copy_to_buf(char *buf)
 		COPY_BUF(buf, *req, ptr);
 	}
 
+	//Copy sharding related flag and array to buffer
+	COPY_BUF(buf, inter_shard_flag, ptr);
+	size_t sizeOfShards = shards_involved.size();
+	cout<<"Size of shard: "<<sizeOfShards<<endl;
+	COPY_BUF(buf, sizeOfShards, ptr);
+	for (uint64_t i = 0; i < sizeOfShards; i++)
+	{
+		uint64_t shard = shards_involved[i];
+		COPY_BUF(buf, shard, ptr);
+	}
+	//End of copy
 	COPY_BUF(buf, return_node, ptr);
 
 	assert(ptr == get_size());
@@ -566,8 +586,8 @@ uint64_t ClientQueryMessage::get_size()
 	size += sizeof(client_startts);
 	size += sizeof(size_t);
 	size += sizeof(uint64_t) * partitions.size();
-	size += sizeof(inter_shard_flag);
-	size += sizeof(uint64_t) * shards_involved.size();
+	//size += sizeof(inter_shard_flag);
+	//size += sizeof(uint64_t) * shards_involved.size();
 	return size;
 }
 
